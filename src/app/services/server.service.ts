910cae021e37372bddeab1a383b4c1ede8418f2b
Tifanny Pariona <tifff2305@gmail.com>
2025-04-30 07:40:02 -0400

    render2

--- src/app/services/server.service.ts
+++ src/app/services/server.service.ts
@@ -21,7 +21,7 @@ export class ServerService {
     const token = localStorage.getItem('authToken');
   
     if (token) {
-      this.socket = io('http://localhost:3000', {
+      this.socket = io('https://examen1-swb.onrender.com', {
         autoConnect: false,
         auth: {
           token: token  // Enviar el token en el objeto auth
